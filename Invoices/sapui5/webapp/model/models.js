bebbcb9101c1809424e5e07dcff4d92cf1559995
diegoizu <diego.izurieta@kfc.com.ec>
2021-10-18 19:08:03 +0000

    prueba stage y commit git

--- Invoices/sapui5/webapp/model/models.js
+++ Invoices/sapui5/webapp/model/models.js
@@ -14,7 +14,7 @@ sap.ui.define([
             createRecipient: function () {
                 var oData = {
                     recipient : {
-                        name: "World"
+                        name: "Natasha"
                     }
                 };
 
