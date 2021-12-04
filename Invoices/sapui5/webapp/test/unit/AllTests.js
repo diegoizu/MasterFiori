7b2ffee7da61cb3e3d59d59aed5882f21aba2414
diegoizu <diego.izurieta@kfc.com.ec>
2021-12-04 17:14:37 +0000

    4 de diciembre 2021, 1

---
+++ Invoices/sapui5/webapp/test/unit/AllTests.js
@@ -0,0 +1,6 @@
+sap.ui.define([
+    'kfc/sapui5/test/unit/model/InvoicesFormatter'
+], function() {
+    'use strict';
+    
+});
---
+++ Invoices/sapui5/webapp/test/unit/unitTests.qunit.js
@@ -0,0 +1,12 @@
+/*Global QUnit*/
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+    "use strict";
+
+    sap.ui.require([
+        'kfc/sapui5/test/unit/AllTests'
+    ], function () {
+        QUnit.start();
+    })
+})
