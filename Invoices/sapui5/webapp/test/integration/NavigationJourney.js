e97258d55ecd3c9f95bb22906d11918287600e5a
diegoizu <diego.izurieta@kfc.com.ec>
2021-12-05 18:14:28 +0000

    05 diciembre 2021 Master Fiori

---
+++ Invoices/sapui5/webapp/test/integration/NavigationJourney.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+    'kfc/sapui5/localService/mockserver',
+    'sap/ui/test/opaQunit',
+    './pages/HelloPanel'
+],/**
+ * 
+ * @param {typeof sap.ui.test.opaQunit} opaQunit 
+ */ 
+function(mockserver, opaQunit) {
+    'use strict';
+
+    QUnit.module("Navigation");
+
+    opaQunit("Should open the Hello Dialog", function(Given, When, Then){
+
+        //Initialize the mock server
+        mockserver.init();
+
+        //Arrangements
+        Given.iStartMyUIComponent({
+            componentConfig: {
+                name: "kfc.sapui5"
+            }
+        });
+
+
+        //Actions
+        When.onTheAppPage.iSayHelloDialogButton();
+
+        //Assertions
+        Then.onTheAppPage.iSeeTheHelloDialog(),
+
+        //Cleanup
+        Then.iTeardownMyApp();
+    })
+    
+});
