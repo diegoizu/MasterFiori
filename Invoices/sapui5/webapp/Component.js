0f46abf24f03f3b40117147a998f33137f1e481f
diegoizu <diego.izurieta@kfc.com.ec>
2022-01-15 17:19:26 +0000

    Al 15Enero2022

--- Invoices/sapui5/webapp/Component.js
+++ Invoices/sapui5/webapp/Component.js
@@ -2,15 +2,17 @@ sap.ui.define([
     "sap/ui/core/UIComponent",
     "kfc/sapui5/model/models",
     "sap/ui/model/resource/ResourceModel",
-    "./controller/HelloDialog"
+    "./controller/HelloDialog",
+    "sap/ui/Device"
 
 ],
     /**
      * 
      * @param {typeof sap.ui.core.UIComponent} UIComponent 
+     * @param {typeof sap.ui.Device} Device
      */
 
-    function (UIComponent, models, ResourceModel, HelloDialog) {
+    function (UIComponent, models, ResourceModel, HelloDialog, Device) {
         'use strict';
 
         return UIComponent.extend("kfc.sapui5.Component", {
@@ -32,6 +34,8 @@ sap.ui.define([
                 //var i18nModel = new ResourceModel({ bundleName: "kfc.sapui5.i18n.i18n" });
                 //this.setModel(i18nModel, "i18n");
 
+                this.setModel(models.createDeviceModel(), "device");
+
                 this._helloDialog = new HelloDialog(this.getRootControl());
 
                 //Create views based on url/hash
@@ -46,6 +50,15 @@ sap.ui.define([
 
             openHelloDialog: function () {
                 this._helloDialog.open();
+            },
+
+            getContentDensityClass: function() {
+                if (!Device.support.touch) {
+                    this._sContentDensityClass = "sapUiSizeCompact";
+                } else {
+                    this._sContentDensityClass = "sapUiSizeCozy";
+                }
+                return this._sContentDensityClass;
             }
         });
 
